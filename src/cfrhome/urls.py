550348130f5f8cdbb9f20d42c8bfefa33120a52b
Nadeem-Maxlearn <nadeem31baig@gmail.com>
2024-07-20 20:15:44 +0530

    added login and register functionality

--- src/cfrhome/urls.py
+++ src/cfrhome/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import home_page_view
+from auth import views as auth_views
 urlpatterns = [
     path('', home_page_view),
+    path('login/', auth_views.login_view),
+    path("register/", auth_views.register_view),
     path('admin/', admin.site.urls),
 ]
