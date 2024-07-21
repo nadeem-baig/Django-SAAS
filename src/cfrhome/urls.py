4f784d659a24927ab2ed16f24c1c27be87efeff9
Nadeem-Maxlearn <nadeem31baig@gmail.com>
2024-07-21 17:50:51 +0530

    Protected routes

--- src/cfrhome/urls.py
+++ src/cfrhome/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
 
-from .views import home_page_view
+from .views import home_page_view,staff_page_view
 from auth import views as auth_views
+
+
 urlpatterns = [
     path('', home_page_view,name='home'),
+    path('staff',staff_page_view),
     path('login/', auth_views.login_view),
     path("register/", auth_views.register_view),
     path('admin/', admin.site.urls),
--- src/cfrhome/views.py
+++ src/cfrhome/views.py
@@ -1,8 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.decorators import login_required
 from visits.models import PageVisits
+from django.contrib.admin.views.decorators import staff_member_required
+
+@login_required
 def home_page_view(request, *args, **kwargs):
+    my_title = "My Title"
+    queryset = PageVisits.objects.all()
+    my_context = {
+        "title": my_title,
+        "page_visits_count": queryset.count()
+    }
+    PageVisits.objects.create()
+    return render(request, 'home.html',my_context)
+
+@staff_member_required
+def staff_page_view(request, *args, **kwargs):
     my_title = "My Title"
     queryset = PageVisits.objects.all()
     my_context = {
